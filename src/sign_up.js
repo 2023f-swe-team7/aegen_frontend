a9db230f6d11e414611bc1e64d836654e8aafb31
rinapark19 <rinapark19@gmail.com>
2023-11-17 11:45:55 +0900

    sign up

--- src/sign_up.js
+++ src/sign_up.js
@@ -3,6 +3,34 @@ import './sign_up.css';
 function SignUp() {
   return (
     <div className="SignUp">
+        <div className='header'>
+            <h2>AEGEN</h2>
+        </div>
+        <div className="signupContainer">
+            <div className="infoContainer">
+                <h3>이름</h3>
+                <input></input>
+                <h3>수업</h3>
+                <input></input>
+                <h3>학번</h3>
+                <input></input>
+                <h3> </h3>
+                <input></input>
+                <h3>이메일</h3>
+                <input></input>
+                <h3> </h3>
+                <input></input>
+                <h3>아이디</h3>
+                <input></input>
+                <h3> </h3>
+                <input></input>
+                <h3>비밀번호</h3>
+                <input></input>
+                <h3> </h3>
+                <input></input>
+            </div>
+            
+        </div>
     </div>
   );
 }
