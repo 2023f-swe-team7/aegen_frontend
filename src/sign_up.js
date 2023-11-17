0f9c9fb6bcc665d86e9fd187dc6bf293a01cbb20
sunwoongskku <sunwoong89@g.skku.edu>
2023-11-17 19:12:44 +0900

    design update

--- src/sign_up.js
+++ src/sign_up.js
@@ -10,23 +10,23 @@ function SignUp() {
         </div>
         <div className="signupContainer">
             <div className="infoContainer">
-                <h3>이름</h3>
+                <h3>이름 :</h3>
                 <input></input>
-                <h3>수업</h3>
+                <h3>수업 :</h3>
                 <input></input>
-                <h3>학번</h3>
-                <input></input>
-                <h3> </h3>
-                <input></input>
-                <h3>이메일</h3>
+                <h3>학번 :</h3>
                 <input></input>
                 <h3> </h3>
                 <input></input>
-                <h3>아이디</h3>
+                <h3>이메일 :</h3>
                 <input></input>
                 <h3> </h3>
                 <input></input>
-                <h3>비밀번호</h3>
+                <h3>아이디 :</h3>
+                <input></input>
+                <h3> </h3>
+                <input></input>
+                <h3>비밀번호 :</h3>
                 <input></input>
                 <h3> </h3>
                 <input></input>
