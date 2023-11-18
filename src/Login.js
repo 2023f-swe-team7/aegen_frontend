49064e9c677f4f810ab7b85c6f93d1eb4bdca5d3
rinapark19 <rinapark19@gmail.com>
2023-11-18 20:26:26 +0900

    dummy data

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,57 @@
 import logo from './img/swe_logo.png';
 import './Login.css';
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import axios from 'axios';
 
 function Login() {
+  const [id, setId] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async() => {
+    try {
+      const response = await axios.post("http://aegen.skku.ac.kr/v1/auth/login", {
+        id: id,
+        password: password,
+      });
+
+      console.log("로그인 성공", response.data);
+    } catch(error) {
+      console.error("로그인 실패", error.message);
+    }
+  };
+
+  const handleLogin1 =() => {
+    console.log(id);
+    console.log(password);
+    if(id == "test" && password == "1234") {
+      navigate("/main");
+    } else {
+      alert("로그인 실패");
+    }
+  }
+
   return (
     <div className="Login">
         <div className='loginContainer'>
             <img src={logo}/>
             <div className="inputContainer">
                 <h2>AEGEN</h2>
-                <input placeholder='ID'></input>
-                <input placeholder='password'></input>
+                <input 
+                type = "text"
+                placeholder='ID'
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+                ></input>
+                <input
+                type="password" 
+                placeholder='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                ></input>
                 <div className="buttonContainer">
-                  <Link to="/main">
-                    <button>로그인</button>
-                  </Link>
+                  <button onClick={handleLogin1}>로그인</button>
                   <Link to="/signup">
                     <button>회원가입</button>
                   </Link>
--- src/sign_up.js
+++ src/sign_up.js
@@ -1,8 +1,32 @@
 import './sign_up.css';
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 function SignUp() {
+  const [name, setName] = useState('');
+  const [subject, setSubject] = useState('');
+  const [studentId, setStudentId] = useState('');
+  const [email, setEmail] = useState('');
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async() => {
+    try {
+      const response = await axios.post("http://aegen.scg.skku.ac.kr/v1/auth", {
+        name: name,
+        subject: subject,
+        studentId: studentId,
+        email: email,
+        id: id,
+        password: password,
+      });
+      console.log("회원가입 성공", response.data);
+    } catch(error) {
+      console.error("회원가입 실패", error.message);
+    }
+  };
+
   return (
     <div className="SignUp">
         <div className='header'>
@@ -11,23 +35,27 @@ function SignUp() {
         <div className="signupContainer">
             <div className="infoContainer">
                 <h3>이름 :</h3>
-                <input></input>
+                <input value={name} onChange={(e) => setName(e.target.value)}></input>
                 <h3>수업 :</h3>
-                <input></input>
+                <input value={subject} onChange={(e) => setSubject(e.target.value)}></input>
                 <h3>학번 :</h3>
-                <input></input>
+                <input value={studentId} onChange={(e) => setStudentId(e.target.value)}></input>
                 <h3> </h3>
                 <input></input>
                 <h3>이메일 :</h3>
-                <input></input>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
                 <h3> </h3>
                 <input></input>
                 <h3>아이디 :</h3>
-                <input></input>
+                <input value={id} onChange={(e) => setId(e.target.value)}></input>
                 <h3> </h3>
                 <input></input>
                 <h3>비밀번호 :</h3>
-                <input></input>
+                <input
+                  tyle="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></input>
                 <h3> </h3>
                 <input></input>
             </div>
